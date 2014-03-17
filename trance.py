fe62d3e10ed0518d41d1d55e4320bc0c67ee2f42
Romain Deterre <romain.deterre@gmail.com>
2014-03-16 20:37:00 -0400

    [WIP] Add 'Variable' class. Code is really broken now. :)

--- trance.py
+++ trance.py
@@ -1,22 +1,38 @@
 import sympy as sp
+import numpy as np
 
 class Node:
-    node_count = 0;
     def __init__(self, time_step):
-        self.node_count = Node.node_count++
         self.dt = time_step
 
 class Port:
-    port_count = 0
-    def __init__(self):
-        self.port_count = Node.port_count++
+    pass
 
 class Electrical_port(Port):
     def __init__(self):
-        self.ni = sp.Symbol('ni{0}'.format(port_count))
-        self.nv = sp.Symbol('nv{0}'.format(port_count))
-        self.ni = sp.Symbol('ni{0}'.format(port_count))
-        self.nv = sp.Symbol('nv{0}'.format(port_count))
+        self.i = Variable('i', 0)
+        self.v = Variable('v', 0)
+
+class Variable:
+    variable_count = 0
+    def __init__(self, name, derivative_order):
+        self.name = name
+        self.der_order = derivative_order
+        self.symbols = [sp.Symbol(name + "_" str(variable_count++) + "_" + str(i) for i in range(derivative_order + 1))]
+        self.values = []
+
+    def relations(self, step_number):
+        # Fill values with void until 'step_number - 1'
+        while len(self.values) < step_number:
+            print("Fill element %d of %s variable" %{len(self.values), name})
+            self.values.append(np.nan)
+
+        # Return useful values
+        rel = []
+        for i in range(1, derivative_order):
+            if not self.values[step_number - i].isNan():
+                rel.append(self.symbols[-i] - self.values[step_number - i])
+        return rel
 
 class Capacitor(Node):
     """
@@ -33,17 +49,24 @@ class Capacitor(Node):
                   I(t) = C * (V(t) - V(t - dt)) / dt
     """
     def __init__(self, capacitance):
-        self.nq = sp.Symbol('nq{0}'.format(node_count))
-        self.oq = sp.Symbol('oq{0}'.format(node_count))
+        self.q = Variable('q', 1)
         self.c = capacitance
-        self.minus_port = Electrical_port()
-        self.plus_port  = Electrical_port()
+        self.ports = [Electrical_port() for i in range(2)]
 
-    def relations(self):
+    def relations(self, step_number):
         rel = []
-        rel.append(self.minus_port.ni = (self.nq - self.oq) / self.dt)
-        rel.append(self.minus_port.ni = self.c * (self.nv - self.ov) / self.dt)
-        rel.append(self.minus_port.ni + self.plus_port.ni)
+        # Relation 1
+        rel.append(self.ports[0].i.symbols[0] - (self.q.symbols[0] - self.q.symbols[-1]) / self.dt)
+        # Relation 2 - Hang in there
+        vt = self.ports[1].symbols[0] - self.ports[0].symbols[0]
+        vtmdt = self.ports[1].symbols[-1] - self.ports[0].symbols[-1]
+        rel.append(self.ports[0].i.symbols[0] - self.c * (vt - vtmdt) / self.dt)
+        # Relation 3 - Relax
+        rel.append(self.ports[0].symbols[0] = self.ports[1].symbols[0])
+
+        # Relations for q
+        rel += self.q.relations(step_number)
+
         return rel
 
 class Current_source(Node):
@@ -67,10 +90,10 @@ class Electrical_link:
     def relations(self):
         rel = []
         for n in self.ports[1:]:
-            rel.append(self.ports[0].ni = n.ni)
-            rel.append(self.ports[0].oi = n.oi)
-            rel.append(self.ports[0].nv = n.nv)
-            rel.append(self.ports[0].ov = n.ov)
+            rel.append(self.ports[0].ni - n.ni)
+            rel.append(self.ports[0].oi - n.oi)
+            rel.append(self.ports[0].nv - n.nv)
+            rel.append(self.ports[0].ov - n.ov)
         return rel
 
 if __name__ == "__main__":
@@ -78,9 +101,7 @@ if __name__ == "__main__":
         +-----------------+
         |                 v
     +---+----+       +---------+
-    |        |       |         |
     |  Capa  |       |Cur. src.|
-    |        |       |         |
     +--------+       +----+----+
         ^                 |
         +-----------------+
