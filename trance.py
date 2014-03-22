Wrong formulas

trance.py
@@ -2,7 +2,7 @@ import sympy as sp
 import numpy as np
 import matplotlib.pyplot as plt
 
-class Electrical_port():
+class Electrical_port:
     def __init__(self, name):
         self.name = name
         self.min_derivative_order = 0
@@ -14,6 +14,9 @@ class Electrical_port():
         rel += self.i.relations(step_number)
         rel += self.v.relations(step_number)
         return rel
+
+    def variables(self):
+        return [self.i, self.v]
 
     def initialize(self, derivative_order, total_steps,
                    default_value):
@@ -44,28 +47,50 @@ class Variable:
                  for i in range(derivative_order + 1)]
 
     def relations(self, step_number):
-        # print("Variable %s has number of elements %d while asking for relation %d"
-              # % (self.name, len(self.values), step_number))
-
-        if len(self.values) < step_number - 1:
-            # Ahh! Missing initial value!
-            raise Exception("Variable %s is missing initial value at step #%d"
-                            % (self.name, step_number))
-
         # Return useful values
         rel = []
         for i in range(1, self.derivative_order + 1):
-            # print("Give relation for variable %s" % self.name)
             rel.append(self.symbols[-i] - self.values[step_number - i])
         return rel
 
-class Capacitor():
+class Node:
+    def __init__(self, name):
+        self.name = name
+        self.vars = {}
+        self.ports = []
+
+    def variables(self):
+        vs = []
+        for v in self.vars:
+            vs.append(self.vars[v])
+        for p in self.ports:
+            vs += p.variables()
+        return vs
+
+    def initialize(self, dt, derivative_order, total_steps, default_value):
+        if derivative_order < self.min_derivative_order:
+            raise Exception("Needs derivative order higher than %d"
+                            % self.min_derivative_order)
+        self.dt = dt
+        for v in self.vars.values():
+            v.initialize(derivative_order, total_steps, default_value)
+        for p in self.ports:
+            p.initialize(derivative_order, total_steps, default_value)
+            
+    def relations(self, step_number):
+        rel = []
+        for p in self.ports:
+            rel += p.relations(step_number)
+        return rel;
+
+class Capacitor(Node):
     """
     Model for an ideal Capacitor.
     Relations used are the following :
     1. I(t) = dQ(t)/dt
     2. I(t) = C dV(t)/dt
     3. I_in(t) = I_out(t)
+    4. Q(t) = C * V(t)
 
     Relation 1 translates to the following once discretised :
                     I(t) = (Q(t) - Q(t - dt)) / dt
@@ -74,35 +99,14 @@ class Capacitor():
                   I(t) = C * (V(t) - V(t - dt)) / dt
     """
     def __init__(self, capacitance, name):
+        Node.__init__(self, name)
         self.min_derivative_order = 1
-        self.name = name
-        self.vars = {}
         self.vars['q'] = Variable('q')
         self.c = capacitance
         self.ports = [Electrical_port("%s.p%d" % (self.name, i))
                       for i in range(2)]
 
-    def variables():
-        vs = []
-        for v in self.vars:
-            vs.append(self.vars[v])
-        return vs
-
-    def initialize(self, dt, derivative_order, total_steps,
-                   default_value):
-        if derivative_order < self.min_derivative_order:
-            raise Exception("Needs derivative order higher than %d"
-                            % self.min_derivative_order)
-        self.dt = dt
-        for v in self.vars:
-            self.vars[v].initialize(derivative_order, total_steps, default_value)
-        for p in self.ports:
-            p.initialize(derivative_order, total_steps, default_value)
-        
     def v(self, time):
-        """
-        Associations are  V(t - n * dt) -> v(-n) 
-        """
         return self.ports[1].v.symbols[time] - self.ports[0].v.symbols[time]
         
     def relations(self, step_number):
@@ -111,99 +115,72 @@ class Capacitor():
         q = self.vars['q']
         rel.append(self.ports[0].i.symbols[0] - (q.symbols[0] - q.symbols[-1]) / self.dt)
         # Relation 2 - Hang in there
-        rel.append(self.ports[0].i.symbols[0] - self.c * (self.v(0) - self.v(-1)) / self.dt)
+        # rel.append(self.ports[0].i.symbols[0] - self.c * (self.v(0) - self.v(-1)) / self.dt)
         # Relation 3 - Relax
         rel.append(self.ports[0].i.symbols[0] + self.ports[1].i.symbols[0])
+        # Relation 4
+        rel.append(q.symbols[0] + (self.c * self.v(0)))
         # Relations for q
         rel += self.vars['q'].relations(step_number)
 
         # Relations for old I and V values
         rel += self.ports[0].relations(step_number)
         rel += self.ports[1].relations(step_number)
-        
         return rel
 
-    def variables(self):
-        vars = [self.vars['q']]
-        for p in self.ports:
-            vars.append(p.i)
-            vars.append(p.v)
-        return vars
-
-class Current_source():
+class Current_source(Node):
     """
     Model for an ideal current source.
     """
     def __init__(self, current, name):
+        Node.__init__(self, name)
         self.min_derivative_order = 0
-        self.name = name
         self.i = current
         self.ports = [Electrical_port("%s.p%d" % (self.name, i))
                       for i in range(2)]
-
-    def initialize(self, dt, derivative_order, total_steps,
-                   default_value):
-        if derivative_order < self.min_derivative_order:
-            raise Exception("Needs derivative order higher than %d"
-                            % self.min_derivative_order)
-        for p in self.ports:
-            p.initialize(derivative_order, total_steps, default_value)
 
     def relations(self, step_number):
         rel = []
         rel.append(self.ports[1].i.symbols[0]
                    + self.ports[0].i.symbols[0])
         rel.append(self.ports[0].i.symbols[0] - self.i)
-
          # Relations for old I and V values
         rel += self.ports[0].relations(step_number)
         rel += self.ports[1].relations(step_number)
-        
         return rel
 
-    def variables(self):
-        vars = []
-        for p in self.ports:
-            vars.append(p.i)
-            vars.append(p.v)
-        return vars
-
-class Voltage_source():
-    """
-    Model for an ideal voltage source.
-    """
-    def __init__(self, voltage, name):
+class Resistance(Node):
+    def __init__(self, resistance, name):
+        Node.__init__(self, name)
         self.min_derivative_order = 0
-        self.name = name
-        self.v = voltage
-        self.ports = [Electrical_port("%s.p" % self.name)]
-
-    def initialize(self, dt, derivative_order, total_steps,
-                   default_value):
-        if derivative_order < self.min_derivative_order:
-            raise Exception("Needs derivative order higher than %d"
-                            % self.min_derivative_order)
-
-        self.ports[0].initialize(derivative_order,
-                                 total_steps,
-                                 default_value)
+        self.r = resistance
+        self.ports = [Electrical_port("%s.p%d" % (self.name, i))
+                      for i in range(2)]
 
     def relations(self, step_number):
         rel = []
-        rel.append(self.ports[0].v.symbols[0] - self.v)
+        v0 = self.ports[0].v.symbols[0]
+        v1 = self.ports[1].v.symbols[0]
+        i0 = self.ports[0].i.symbols[0]
+        i1 = self.ports[1].i.symbols[0]
+        rel.append(i0 + i1)
+        rel.append((v1 - v0) + (self.r * i1))
+        rel += Node.relations(self, step_number)
+        return rel
 
+class Ground(Node):
+    def __init__(self, name):
+        Node.__init__(self, name)
+        self.min_derivative_order = 0
+        self.ports = [Electrical_port("%s.p" % self.name)]
+
+    def relations(self, step_number):
+        rel = []
+        rel.append(self.ports[0].v.symbols[0])
         # Relations for old I and V values
         rel += self.ports[0].relations(step_number)
         return rel
-
-    def variables(self):
-        vars = []
-        for p in self.ports:
-            vars.append(p.i)
-            vars.append(p.v)
-        return vars
         
-
 class Electrical_link:
     def __init__(self, ports):
         self.ports = ports
@@ -241,8 +218,24 @@ class Simulation:
 
     def simulate(self):
         for st in range(self.derivative_order, self.total_time_steps):
+            print("Solve time step %d" % st)
             self.solve(st)
 
+    def dprint(self, symbols, rel, results):
+        """
+        For debug purposes, format and prints on screen solving
+        parameters.
+        """
+        print("------------------------------------SYMBOLS------------------------------------")
+        for s in symbols:
+            print(s)
+        print("-----------------------------------RELATIONS-----------------------------------")
+        for r in rel:
+            print(r)
+        print("------------------------------------RESULTS------------------------------------")
+        for r in results:
+            print(r)
+            
     def solve(self, time_step):
         # Concatenate relations.
         rel = []
@@ -256,63 +249,18 @@ class Simulation:
         for v in var:
             for i in range (self.derivative_order + 1):
                 symbols.append(v.symbols[i])
+
+        print("Try to solve %d symbols." % len(symbols))
         results = sp.solve(rel, symbols)
         if not len(results) == len(symbols):
-            raise Exception("The solver failed to deduce all symbols")
-        print("Results are : %s" % results.values())
+            self.dprint(symbols, rel, results)
+            raise Exception("Solver only deduced %d symbols"
+                            % len(results))
         for v in var:
             v.values[time_step] = results[v.symbols[0]]
-            # = np.append(v.values, results[v.symbols[0]])
 
     def initialize(self, dt, total_time_steps, default_value):
         self.total_time_steps = total_time_steps
         for n in self.nodes:
             n.initialize(dt, self.derivative_order,
                          total_time_steps, default_value)
-
-        
-if __name__ == "__main__":
-
-    """
-      +-------+
-      |       |
-    +-+-+   +-+--+
-    |Cur|   |Capa|
-    +-+-+   +-+--+
-      |       |
-      +---+---+
-          |
-        +-+-+
-        |Gnd|
-        +---+
-    """
-    print("This is the Bridge test program.")
-    cur = Current_source(1, "cur", 1)
-    cap = Capacitor(1e-3, 0.1, "cap", 1)
-    gnd = Voltage_source(0, "gnd", 1)
-    l1 = Electrical_link()
-    l1.ports += [cur.ports[1], cap.ports[0], gnd.port]
-    l2 = Electrical_link()
-    l2.ports += [cur.ports[0], cap.ports[1]]
-    
-    # Initial values
-    cap.q.values = np.append(cap.q.values, 1)
-    cap.ports[0].v.values = np.append(cap.ports[0].v.values, 0)
-    cap.ports[0].i.values = np.append(cap.ports[0].i.values, 0)
-    cap.ports[1].v.values = np.append(cap.ports[1].v.values, 0)
-    cap.ports[1].i.values = np.append(cap.ports[1].i.values, 0)
-    cur.ports[0].v.values = np.append(cur.ports[0].v.values, 0)
-    cur.ports[0].i.values = np.append(cur.ports[0].i.values, 0)
-    cur.ports[1].v.values = np.append(cur.ports[1].v.values, 0)
-    cur.ports[1].i.values = np.append(cur.ports[1].i.values, 0)
-    gnd.port.i.values = np.append(gnd.port.i.values, 0)
-    gnd.port.v.values = np.append(gnd.port.v.values, 0)
-    els = [cur, cap, gnd, l1, l2]
-    try:
-        for i in range(1, 2):
-            solve(i, els, 1)
-    except TypeError:
-        print("Type error while solving")
-
-    plt.plot(cap.q.values)
-    plt.show()                  